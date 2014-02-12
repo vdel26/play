b99cbe5a3e65d33df9a47d7230623a6a982a466a
Victor Delgado <vdel26@gmail.com>
2014-02-12 11:46:17 +0100

    styling tab picker

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
 function onClick () {
-   chrome.runtime.sendMessage({ method: 'browserActionSetUp'});
+   // chrome.runtime.sendMessage({ method: 'browserActionSetUp'});
+   chrome.tabs.query({ currentWindow: true }, createTabList);
+}
+
+function createTabList (tabList) {
+    var ul = document.querySelector('ul');
+    tabList.forEach(function (tab) {
+        // create document fragment <li class='current'><li>
+        var newItem = document.createElement('li');
+        newItem.textContent = tab.title;
+        ul.appendChild(newItem);
+    });
+}
+
+function getMediaTab () {
+    // send message to eventjs to see if there is
+    // a media tab currently set
 }
 
 document.addEventListener('DOMContentLoaded', function () {
